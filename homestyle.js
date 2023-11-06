980b89f53defd1fa0c7fb7fcddd4e005966687ff
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-06 11:54:36 -0300

    ac1.2

--- homestyle.js
+++ homestyle.js
@@ -2,96 +2,105 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
-
-
+ 
+ 
 //configurando o roteamento para teste no postman
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-const port = 3000;
-
-
+const port = 3001;
+ 
+ 
 //configurando o acesso ao mongodb
 mongoose.connect('mongodb://127.0.0.1:27017/homestyle',
 {   useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
-
+ 
+ 
 //criando a model do seu projeto
-const UsuarioSchema = new mongoose.Schema({
+const usuarioSchema = new mongoose.Schema({
     email : {type : String, required : true},
     senha : {type : String}
 });
-
-
-const Usuario = mongoose.model("Usuario", UsuarioSchema);
-
-const ProdutodecoracaoSchema = new mongoose.Schema({
-    id_produtodecoracao: {type : String, required : true},
-    Descricao: {type : String},
-    Fornecedor: {type : String},
-    Data_fabricacao: {type : Date},
-    Quantidade_estoque: {type : Number}
+ 
+ 
+const Usuario = mongoose.model("Usuario", usuarioSchema);
+ 
+ 
+//criando a segunda model
+const produtodecoracaoSchema = new mongoose.Schema({
+    id_produtodecoracao : {type : String, required : true},
+    descricao : {type : String},
+    fornecedor : {type : String},
+    dataFabricacao : {type : Date},
+    quantidadeEstoque : {type : Number}
 });
-
-
-const Produtodecoracao = mongoose.model("Produtodecoracao", ProdutodecoracaoSchema);
-
-
+ 
+ 
+const produtodecoracao = mongoose.model("produtodecoracao", produtodecoracaoSchema);
+ 
+ 
 //configurando os roteamentos
 app.post("/cadastrousuario", async(req, res)=>{
     const email = req.body.email;
-    const senha = req.body.senha
-
-    const Usuario = new Usuario({
+    const senha = req.body.senha;
+ 
+ 
+//mandando para banco
+    const usuario = new Usuario({
         email : email,
-        senha : senha
+        senha : senha,
     })
-
-
+ 
     try{
-        const newUsuario = await Usuario.save();
-        res.json({error : null, msg : "Cadastro ok", pessoaId : newUsuario._id});
+        const newUsuario = await usuario.save();
+        res.json({error : null, msg : "Cadastro ok", usuarioId : newUsuario._id});
     } catch(error){
         res.status(400).json({error});
     }
-
-
 });
-
+ 
 app.post("/cadastroprodutodecoracao", async(req, res)=>{
     const id_produtodecoracao = req.body.id_produtodecoracao;
-    const Descricao = req.body.Descricao
-    const Fornecedor = req.body.Fornecedor
-    const Data_fabricacao = req.body.Data_fabricacao
-    const Quantidade_estoque = req.body.Quantidade_estoque  
-
-    const Usuario = new Usuario({
-        id_produtodecoracao: id_produtodecoracao,
-        Descricao: Descricao ,
-        Fornecedor: Fornecedor,
-        Data_fabricacao: Data_fabricacao,
-        Quantidade_estoque: Quantidade_estoque
+    const descricao = req.body.descricao;
+    const fornecedor = req.body.marca;
+    const dataFabricacao = req.body.dataFabricacao;
+    const quantidadeEstoque = req.body.quantidadeEstoque
+ 
+    //mandando para banco
+    const produtodecoracao = new produtodecoracao({
+        id_produtodecoracao : id_produtodecoracao,
+        descricao : descricao,
+        fornecedor : fornecedor,
+        dataFabricacao : dataFabricacao,
+        quantidadeEstoque : quantidadeEstoque
     })
-
-
+ 
     try{
-        const newProdutodecoracao = await Produtodecoracao.save();
-        res.json({error : null, msg : "Cadastro ok", });
+        const newprodutodecoracao = await produtodecoracao.save();
+        res.json({error : null, msg : "Cadastro ok", produtodecoracaoId : newprodutodecoracao._id});
     } catch(error){
         res.status(400).json({error});
     }
-
-
 });
-
+ 
+//rota para o get de cadastro
+app.get("/cadastrousuario", async(req, res)=>{
+    res.sendFile(__dirname +"/cadastrousuario.html");
+})
+ 
+//rota para o get de cadastro
+app.get("/cadastroprodutodecoracao", async(req, res)=>{
+    res.sendFile(__dirname +"/cadastroprodutodecoracao.html");
+})
+ 
+//rota raiz - inw
 app.get("/", async(req, res)=>{
     res.sendFile(__dirname +"/index.html");
-})
-
-
+});
+ 
 //configurando a porta
 app.listen(port, ()=>{
     console.log(`Servidor rodando na porta ${port}`);
-})
+});
